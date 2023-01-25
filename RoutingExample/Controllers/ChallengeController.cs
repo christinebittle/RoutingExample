63a8b92e6d9a5675fba9a3b2033096a03d92b56e
Somebody <christinebitts@gmail.com>
2023-01-24 19:49:51 -0500

    Add project files.

---
+++ RoutingExample/Controllers/ChallengeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RoutingExample.Controllers
+{
+    public class ChallengeController : ApiController
+    {
+        /// <summary>
+        /// Calculates the volumne of a prism given length, width, and height
+        /// </summary>
+        /// <param name="x">The width of the prism in cm</param>
+        /// <param name="y">The length of the prism in cm</param>
+        /// <param name="z">The height of the prism in cm</param>
+        /// <returns>
+        /// The total volumne is {result}cm^3
+        /// </returns>
+        //GET: api/challenge/calculate/x/y/z -> "The total volumne is {result}cm^3"
+        [HttpGet]
+        [Route("api/challenge/calculate/{x}/{y}/{z}")]
+        public string calculate(int x, int y, int z)
+        {
+            int volume = x * y * z;
+            string message = "The total volumne is "+volume+"cm^3";
+            return message;
+        }
+    }
+}
---
+++ RoutingExample/Controllers/RouteTestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RoutingExample.Controllers
+{
+    public class RouteTestController : ApiController
+    {
+
+        //get: api/RouteTest/Math/x/y -> x*y
+        [HttpGet]
+        [Route("api/RouteTest/Math/{operand1}/{operand2}")]
+        public int Math(int operand1, int operand2)
+        {
+            return operand1*operand2;
+        }
+
+        //get: api/RouteTest/Greeting2 -> "howdy"
+        [HttpGet]
+        public string Greeting2()
+        {
+
+            return "howdy";
+        }
+
+        //post: api/RouteTest/Greeting3 ->"Heya"
+        [HttpPost]
+        public string Greeting3()
+        {
+            return "Heya";
+        }
+
+        //post: api/RouteTest/Greeting4 -> "Hola"
+        [HttpPost]
+        public string Greeting4()
+        {
+            return "Hola";
+        }
+
+    }
+}
